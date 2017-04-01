2c36d9e9dbc78a571bb917a00e09bb7ea1aa0951
Davis Catherman <Davis Catherman>
2017-04-01 11:19:32 -0400

    count pushes to bucket and footage comes from drone

--- TargetCount.py
+++ TargetCount.py
@@ -1,3 +1,6 @@
+print "Initiallizing..."
+print "Please remember that Drone Footage and Internet is necessary for this code to work."
+
 from collections import deque
 import cv2
 import numpy as np
@@ -12,50 +15,36 @@ from boto.s3.key import Key
 def nothing(x):
     pass
 
+# NOTE: Internet necessary for this part of code to work
 def pushToBucket(count):
     c = boto.connect_s3()
-    b = c.get_bucket('cokecount')
+    b = c.get_bucket('cokecount') 
     k = Key(b)
     k.key = 'count'
     
     k.set_contents_from_string(str(count))
     
-    print k.get_contents_as_string()
+    print "Bucket info: " + k.get_contents_as_string()
     
-    
-cv2.namedWindow('image')
 
-# construct the argument parse and parse the arguments
-ap = argparse.ArgumentParser()
-ap.add_argument("-v", "--video",
-    help="path to the (optional) video file")
-ap.add_argument("-b", "--buffer", type=int, default=64,
-    help="max buffer size")
-args = vars(ap.parse_args())
+#cv2.namedWindow('frame')
 
-pts = deque(maxlen=args["buffer"])
+cam = cv2.VideoCapture('tcp://192.168.1.1:5555')
 
-# cap = cv2.VideoCapture("./out.mp4")
-# while not cap.isOpened():
-#     cap = cv2.VideoCapture("./out.mp4")
-#     cv2.waitKey(1000)
-#     print "Wait for the header"
-     
-cap = cv2.VideoCapture(0)
- 
-ret, prev = cap.read()
-prevgray = cv2.cvtColor(prev, cv2.COLOR_BGR2GRAY)
+ret = True
+
 show_hsv = False
 show_glitch = False
-cur_glitch = prev.copy()
 
-while(1):
+print "Camera capture initialized, entering loop..."
+
+while(True):
     obj = []
-    
-    ret, frame = cap.read()
-    
-    #frame = cv2.imread("./img.png")
-    #frame = cv2.resize(frame, (0,0), fx=0.5, fy=0.5)
+    ret, frame = cam.read()
+    if not ret:
+        print "Trying again to read from camera..."
+        time.sleep(.1)
+        continue
     
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
@@ -76,7 +65,7 @@ while(1):
     #make array for final values
     HSVLOW=np.array([hul,sal,val])
     HSVHIGH=np.array([huh,sah,vah])
-
+    
     #apply the range on a mask
     mask = cv2.inRange(hsv,HSVLOW, HSVHIGH)
     
@@ -101,13 +90,18 @@ while(1):
                 obj.append((x,y))
                 cv2.circle(Gaussianframe, (int(x), int(y)), int(radius),(0, 255, 255), 2)
                 cv2.circle(Gaussianframe, center, 1, (0, 0, 255), -1)
-            
+    
     pushToBucket(len(obj))
+    
     cv2.imshow('image', res)
     cv2.imshow('blobs', Gaussianframe)
     
     k = cv2.waitKey(5) & 0xFF
     if k == 27:
+        print "Quitting...!"
         break
-
+    
+cam.release()
 cv2.destroyAllWindows()
+
+print "Closed."
