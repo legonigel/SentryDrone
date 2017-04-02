8df04ed8ffecd82434dba102cdf4f7fddb530423
Davis Catherman <Davis Catherman>
2017-04-02 09:12:14 -0400

    non functional base code for new feature

--- main.py
+++ main.py
@@ -1,5 +1,27 @@
 import lib.libardrone
 import boto3
+import boto
+from boto.s3.key import Key
+
+def searchTarget(drone):
+	c = boto.connect_s3()
+	b = c.get_bucket('targetxandydata')
+	k = Key(b)
+	k.key = 'x'
+	x = k.get_contents_as_string()
+	k.key = 'xMax'
+	xMax = k.get_contents_as_string()
+	
+	error = 40
+	
+	if x < xMax/2 - error:
+		# need to turn right
+		print "1"
+	elif x > xMax/2 + error:
+		# need to turn left
+		print "2"
+	
+	if(x )
 
 def main():
 	sqs = boto3.resource('sqs', region_name="us-east-1")
@@ -17,6 +39,8 @@ def main():
 				body = m.body
 				if body == "reset":
 					body = "emergency"
+				if body == "search":
+					searchTarget(drone)
 				drone.apply_command(body)
 				m.delete()
 	except (KeyboardInterrupt, SystemExit):
